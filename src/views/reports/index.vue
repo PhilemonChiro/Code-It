db5790f5b3ce1c236346703d3ffb1453756608eb
PhilemonChiro <49866367+PhilemonChiro@users.noreply.github.com>
2020-08-12 08:35:30 +0200

    'Code-It'

---
+++ src/views/reports/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>reports</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style >
+	#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
